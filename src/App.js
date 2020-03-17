36992e8e9a5ab52eb67401c976078c40e40a532d
NabazKamal <nabaz.kamal@gmail.com>
2020-03-17 22:49:45 +0100

    Add search component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, {Component} from 'react';
-import {CardList} from './Components/card-list/card-list.component';
+import React, { Component } from "react";
+import { CardList } from "./Components/card-list/card-list.component";
+import { SearchBox } from "./Components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-      ]
+      monsters: [],
+      placeHplaceholderolder:''
     };
   }
-  componentDidMount()
-  {
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(result =>result.json())
-    .then(users=>this.setState({monsters:users}));
+      .then(result => result.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-      <CardList monsters={this.state.monsters}/>
+        <SearchBox placeholder="search monster" />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
---
+++ src/Components/search-box/search-box.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const SearchBox =(props) =>
+{
+    return(
+        <input type="search" placeholder={props.placeholder}/>
+    );
+};
