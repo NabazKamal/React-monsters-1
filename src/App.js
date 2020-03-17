704f01b4227046a30ffb599dd69b6a134ccb45ff
Nabaz Kamal <Nabaz.Kamal@apoteket.se>
2020-03-17 15:53:05 +0100

    Fetch the monsters from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-    <h1>hej hej</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+      ]
+    };
+  }
+  componentDidMount()
+  {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(result =>result.json())
+    .then(users=>this.setState({monsters:users}));
+  }
+  render() {
+    return (
+      <div className="App">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+      </div>
+    );
+  }
 }
-
 export default App;
