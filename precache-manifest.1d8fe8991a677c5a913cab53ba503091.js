d8ac565756fdc8d1dbacf117980d14cf7fc3d078
NabazKamal <nabaz.kamal@gmail.com>
2020-03-21 16:57:54 +0100

    Updates

---
+++ precache-manifest.1d8fe8991a677c5a913cab53ba503091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1584b735d49b27d0e9c620258b6d58",
+    "url": "/NabazKamal/React-monsters-1/index.html"
+  },
+  {
+    "revision": "07f36526fbbcca01fd97",
+    "url": "/NabazKamal/React-monsters-1/static/css/main.25db4689.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/NabazKamal/React-monsters-1/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/NabazKamal/React-monsters-1/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f36526fbbcca01fd97",
+    "url": "/NabazKamal/React-monsters-1/static/js/main.b51b019c.chunk.js"
+  },
+  {
+    "revision": "f92b03307dfd32d438a5",
+    "url": "/NabazKamal/React-monsters-1/static/js/runtime-main.571b1da6.js"
+  }
+]);
